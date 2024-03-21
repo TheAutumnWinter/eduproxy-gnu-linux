e68e08b91b4baf7e101907e85d78c1be094e0a62
Autumn <163492581+TheAutumnWinter@users.noreply.github.com>
2024-03-21 12:30:13 +1100

    Update install.sh
    finish install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
-echo "compiling program"
+echo "compiling survey"
 gcc settings.c -o surveyproxy
 ./surveyproxy
 gcc main.c -o eduproxy-gnu-linux
+mkdir ~/.EDUPROXY
+rm surveyproxy
+mv eduproxy-gnu-linux ~/.EDUPROXY
+mv setting1 ~/.EDUPROXY
+mv setting2 ~/.EDUPROXY
+echo -e "~/.EDUPROXY/eduproxy-gnu-linux" >> ~/.bashrc
+echo "Install complete!"
