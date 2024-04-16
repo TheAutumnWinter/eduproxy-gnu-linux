bdd5f9a78a751910fe6b8e63c0987cd045b23572
Autumn <163492581+TheAutumnWinter@users.noreply.github.com>
2024-04-16 10:42:11 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 echo "compiling survey"
 gcc settings.c -o surveyproxy
+touch settings1
+touch settings2
+touch run.sh
+chmod 777 run.sh
 ./surveyproxy
 gcc main.c -o eduproxy-gnu-linux
 mkdir ~/.EDUPROXY
 rm surveyproxy
 mv eduproxy-gnu-linux ~/.EDUPROXY
-mv setting1 ~/.EDUPROXY
-mv setting2 ~/.EDUPROXY
+mv settings1 ~/.EDUPROXY
+mv settings2 ~/.EDUPROXY
+mv run.sh ~/.EDUPROXY
 echo -e "~/.EDUPROXY/eduproxy-gnu-linux" >> ~/.bashrc
+echo -e " ~/.EDUPROXY/run.sh" >> ~/.bashrc
 echo "Install complete!"
