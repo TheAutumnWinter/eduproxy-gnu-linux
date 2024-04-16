5456dbfc3a37181ef9d73ba359d5c411e19724d1
Autumn <163492581+TheAutumnWinter@users.noreply.github.com>
2024-04-16 10:38:36 +1000

    Update settings.c

--- settings.c
+++ settings.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-
-//This program surveys the questions of the user and stores them into a file before the compiled program is deleted
+#include <string.h>
+//This program surveys the computer stores the results into 2 files before the compiled program is deleted
 
 
 
 void main()
 {
-	printf("\nPlease give the name of the network that uses the proxy\n");
+	printf("\nGetting current wifi IP\n");
 	char network[30];
-	scanf(" %s", &network);
+	FILE *ipget = popen("hostname -i", "r");
+	char buff[30];
+	strcpy(network ,fgets(buff,30, ipget));
 	printf("\nok, so %s is the network then?\n", network);
 	FILE *settings1; 
 	settings1 = fopen("settings1", "w+");
 	FILE* settings2 = fopen("settings2", "w+");
 	fprintf(settings1, "%s", network);
-	fprintf(settings2, "eduproxy:8080");
+	fprintf(settings2, "export http_proxy='eduproxy:8080'  \n  export https_proxy='eduproxy:8080'");
 }
